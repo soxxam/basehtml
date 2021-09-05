93dd3e3cca0e7a49d21aef8cd8cc43304668a07f
Tu Tran <72645410+soxxam@users.noreply.github.com>
2021-09-05 16:39:57 +0700

    Add files via upload

---
+++ base.js
@@ -0,0 +1,9 @@
+function aboutme() {
+    var x = document.getElementById("aboutme");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } 
+    else {
+      x.style.display = "none";
+    }
+  }
